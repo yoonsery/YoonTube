a8c9ece78fe348b62756b70920886407d1a38244
yoonsery <yoon.sery@icloud.com>
2021-05-13 21:39:58 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
-    fetch(googleAPIKey, requestOptions)
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
-    <>
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
-    </>
+    </div>
   );
 }
 
