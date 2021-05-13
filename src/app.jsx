7d1c2e1eca653437b6273987d61941ecfd04f496
yoonsery <yoon.sery@icloud.com>
2021-05-13 16:26:08 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello 😆</h1>;
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(googleAPIKey, requestOptions)
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return (
+    <>
+      <VideoList videos={videos} />
+    </>
+  );
 }
 
 export default App;
