4a064d08fd2df78fb2b77ef0e103046f5954ad01
yoonsery <yoon.sery@icloud.com>
2021-05-18 01:36:41 +0900

    add responsive mode

--- src/app.jsx
+++ src/app.jsx
@@ -7,20 +7,22 @@ import VideoList from './components/video_list/video_list';
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+
   const useTitle = (initialTitle) => {
     const [title, setTitle] = useState(initialTitle);
 
     const updateTitle = () => {
       const htmlTitle = document.querySelector('title');
       htmlTitle.innerHTML = title;
+      console.log(title); // title 이 처음 클릭한 영상의 타이틀로만 고정됨
     };
     useEffect(updateTitle, [title]);
     return setTitle;
   };
 
-  const selectVideo = (video) => {
+  const selectVideo = useCallback((video) => {
     setSelectedVideo(video);
-  };
+  }, []);
 
   const search = useCallback(
     (query) => {
