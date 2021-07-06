b446fe0ecabe0aa5c669ffaadbb3d4c3c20974bb
yoonsery <yunsel2@gmail.com>
2021-07-06 22:35:47 +0900

    edit css & code

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,34 +1,31 @@
-import React, { memo } from 'react';
+import React from 'react';
 import styles from './video_item.module.css';
 import { htmlUnescape } from 'escape-goat';
 
-const VideoItem = memo(
-  ({ video, video: { snippet }, onVideoClick, display }) => {
-    const displayType = display === 'list' ? styles.list : styles.grid;
-    const title =
-      snippet.title.length < 60
-        ? snippet.title
-        : snippet.title.slice(0, 60) + '...';
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === 'list' ? styles.list : styles.grid;
+  const title =
+    snippet.title.length < 60
+      ? snippet.title
+      : snippet.title.slice(0, 60) + '...';
 
-    return (
-      <li
-        className={`${styles.container} ${displayType}`}
-        onClick={() => onVideoClick(video)}
-      >
-        <div className={styles.video}>
-          <img
-            className={styles.thumbnail}
-            src={snippet.thumbnails.medium.url}
-            alt="video thumbnail"
-          />
-          <div className={styles.metadata}>
-            <p className={styles.title}>{htmlUnescape(title)}</p>
-            <p className={styles.channel}>{snippet.channelTitle}</p>
-          </div>
+  return (
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{htmlUnescape(title)}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
         </div>
-      </li>
-    );
-  }
-);
-
+      </div>
+    </li>
+  );
+};
 export default VideoItem;
